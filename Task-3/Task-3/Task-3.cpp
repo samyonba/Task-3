ed8583b02e8bfdeef0a46957628aff1b0c165c9e
samyonba <samyonba@gmail.com>
2021-10-02 01:06:34 +0300

    complete append_average()

---
+++ Task-3/Task-3/Task-3.cpp
@@ -0,0 +1,45 @@
+﻿
+#include <iostream>
+#include <list>
+
+void append_average(std::list<double>& list)
+{
+	if (list.empty())
+		return;
+	double sum = 0;
+	for (auto a : list)
+	{
+		sum += a;
+	}
+	//list.push_back(sum/static_cast<double>(list.size()));
+	list.push_back(sum / list.size());
+}
+
+int main()
+{
+//---------- Задание 1 ----------
+
+    std::list<double> myDoubleList = { 2.2, 3.3, 4.4, 5.5 };
+
+	for (auto i : myDoubleList)
+		std::cout << i << " ";
+	std::cout << std::endl;
+
+    append_average(myDoubleList);
+
+	for (auto i : myDoubleList)
+		std::cout << i << " ";
+	std::cout << std::endl;
+
+//-------------------------------
+
+//---------- Задание 2 ----------
+
+
+//-------------------------------
+
+//---------- Задание 3 ----------
+
+
+//-------------------------------
+}
